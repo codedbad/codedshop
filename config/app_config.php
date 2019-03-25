44c322dcdce710b1e5585aaa36e8dc279e4403ea
Rustams Šteinbergs <47445696+codedbad@users.noreply.github.com>
2019-03-25 15:17:11 +0200

    Add files via upload

--- config/app_config.php
+++ config/app_config.php
@@ -3,9 +3,10 @@
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '110189';
-$dbtbl = 'pr_list';
+$dbname = 'pr_list';
+$dbtble = 'products';
 
-$con = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbtbl");
+$con = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname");
 
 if (!$con) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -13,3 +14,9 @@ if (!$con) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
+
+// $dbhost = '127.0.0.1:3306';
+// $dbuser = 'u652637700_root';
+// $dbpass = 'whoolois1428';
+// $dbname = 'u652637700_dbtbl';
+// $dbtble = 'products';
