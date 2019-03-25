161f43f3a72738b6e88363d8d62c3808be478df4
Rustams Šteinbergs <47445696+codedbad@users.noreply.github.com>
2019-03-25 15:13:56 +0200

    Add files via upload

--- controlpanel.php
+++ controlpanel.php
@@ -1,149 +1,167 @@
+<?php
+session_start([
+    'cookie_lifetime' => 86400,
+    'read_and_close' => true,
+]);
+include_once 'config/app_config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/app.css">
-    <title>Controlpanel : Rustams Šteinbergs</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles/app.css">
+  <title>Controlpanel : codedbad</title>
 </head>
-<header>
+
+<body>
+  <header>
     <a href="https://github.com/codedbad">
-        <div class="codedbad">
-            codedbad
-        </div>
+      <div class="codedbad">
+        codedbad
+      </div>
     </a>
-</header>
-<main>
-<?php
-include_once 'config/app_config.php';               
-error_reporting(E_ALL);
-$succ = "";
+    <a href="index.php">
+      <img class="btn-home-cp" src="images/home.png" alt="">
+    </a>
+  </header>
+  <main>
+    <?php
+        error_reporting(E_ALL);
+        $sucSave = "";
+        $message = "";
+        $saveErorr = "";
 
-$con = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbtbl");
+        if ($con->connect_error) {
+            die("Connection failed: " . $con->connect_error);
+        } 
 
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-} 
+        if(isset($_POST['submit'])) {
+            $getSku = $_POST['sku'];
+            $getMox = $_POST['mox'];
+            $getDim = $_POST['dim'];
+            $getOsx = $_POST['osx'];
+            $getMem = $_POST['mem'];
+            $getScr = $_POST['scr'];
+            $getPrc = $_POST['prc'];
+            $getImg = $_POST['img'];
 
-if(isset($_POST['submit'])) {
-    $getSku = $_POST['sku'];
-    $getMox = $_POST['mox'];
-    $getDim = $_POST['dim'];
-    $getOsx = $_POST['osx'];
-    $getMem = $_POST['mem'];
-    $getScr = $_POST['scr'];
-    $getPrc = $_POST['prc'];
-    $getImg = $_POST['img'];
+            $query = "INSERT INTO products" . "(sku, mox, dim, osx, mem, scr, prc, img)"
+            . "VALUES ('$getSku','$getMox', '$getDim', '$getOsx', '$getMem', '$getScr', '$getPrc', '$getImg')";
 
-    $query = "INSERT INTO products" . "(sku, mox, dim, osx, mem, scr, prc, img)" . "VALUES ('$getSku','$getMox', '$getDim', '$getOsx', '$getMem', '$getScr', '$getPrc', '$getImg')";
-
-    if ($con->query($query) === TRUE) { 
-        $succ = "success";
-    } else {
-        echo "Error: " . $query . "<br>" . $con->error;
-    }
-}
-?>
-<div class="cp_con">
-    <div>
-    <form action="<?php $_PHP_SELF ?>" method="post">
-    <label for="model">SKU:</label><br>
-            <input class="input" type="text" name="sku" id="sku" required="required">
-            <label for="model">Model:</label><br>
-            <input class="input" type="text" name="mox" id="mox" required="required">
-            <label for="dimension">Dimension:</label><br>
-            <input class="input" type="text" name="dim" id="dim" required="required">
-            <label for="os">Operating system:</label><br>
-            <input class="input" type="text" name="osx" id="osx" required="required">
-            <label for="memory">Memory:</label><br>
-            <input class="input" type="text" name="mem" id="mem" required="required">
-            <label for="screen">Screen:</label><br>
-            <input class="input" type="text" name="scr" id="scr" required="required">
-            <label for="price">Price:</label><br>
-            <input class="input" type="text" name="prc" id="prc" required="required">
-            <label for="image">Image:</label><br>
-            <input class="input" type="text" name="img" id="img" required="required">
-    </div>
-</div>
-<div class="container-menu">
-    <div class="menu-right">Save new product:<br>
-        <span class="small-tx">please provide all info</span>
-    </div>
-    <div class="menu-left">
-        <span class="btn-block">
-        <button class="btn-submit" type="submit" name="submit">Save</button>
-        </span><br>
-        <span class="small-tx-rd"><?php echo $succ;?></span>
-    </div>
-</div>
-</form>
-<div class="container-menu">
-    <div class="menu-right">Delete selected:<br>
-        <span class="small-tx">select one or more to delete</span>
-    </div>
-    <div class="menu-left">
-        <span class="btn-block">
-        <button class="btn-delete" type="submit" name="delete">Delete</button>
-        </span><br>
-        <span class="small-tx-rd">success</span>
-    </div>
-</div>
-<div class="container-pl">
-        <?php
-        $query = 'SELECT * from products ORDER by dat';
-        $result = $con->query($query, MYSQLI_STORE_RESULT);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $mox = $row["mox"];
-                $dim = $row["dim"];
-                $osx = $row["osx"];
-                $mem = $row["mem"];
-                $src = $row["scr"];
-                $prc = $row["prc"];
-                $img = $row["img"];
-                ?>
-
-    <div class="portrait">
-        <ul>
-            <li class="small-tx-dg">
-                <input class="checkbox" type="checkbox" name="select" value="delete">
-                <span>Mobile Phones</span>
-            </li>
-        </ul>
-            <img src="<?php echo $img;?>" alt="">
-        <ul>
-            <li class="medium-tx">
-                <span>
-                    <?php echo $mox;?>
-                </span>
-            </li>
-            <li class="small-tx">
-                <span>
-                    <?php echo $mem;?>
-                </span>
-                <span>Gb,</span>
-                <span>
-                    <?php echo $src;?>
-                </span>
-            </li>
-            <li class="price-tag-pl">
-                <span>€</span>
-                <span>
-                    <?php echo $prc;?>
-                </span>
-                <span>Eur</span>
-            </li>
-        </ul>
-    </div>
-        <?php
+            if ($con->query($query) === TRUE) { 
+                $sucSave = "success";
+            } else {
+                $saveErorr = "Error";
             }
-        } else {
-            echo "0 results";
         }
-        mysqli_close($con);
         ?>
-</div>
-</main>
+    <form action="<?php $_PHP_SELF ?>" method="post">
+      <div class="input-cp">
+        <div>
+          <label for="model">SKU:</label><br>
+          <input class="input" type="text" name="sku" id="sku" required="required">
+          <label for="model">Model:</label><br>
+          <input class="input" type="text" name="mox" id="mox" required="required">
+          <label for="dimension">Dimension:</label><br>
+          <input class="input" type="text" name="dim" id="dim" required="required">
+          <label for="os">Operating system:</label><br>
+          <input class="input" type="text" name="osx" id="osx" required="required">
+          <label for="memory">Memory:</label><br>
+          <input class="input" type="text" name="mem" id="mem" required="required">
+          <label for="screen">Screen:</label><br>
+          <input class="input" type="text" name="scr" id="scr" required="required">
+          <label for="price">Price:</label><br>
+          <input class="input" type="text" name="prc" id="prc" required="required">
+          <label for="image">Image:</label><br>
+          <input class="input" type="text" name="img" id="img" required="required">
+        </div>
+      </div>
+      <div class="container-menu-cp">
+        <div>
+          <span class="medium-tx">Save new product:</span><br>
+          <span class="small-tx">please provide all info</span>
+        </div>
+        <div>
+          <span class="btn-block">
+            <button class="btn-submit" type="submit" name="submit">Save</button>
+            <span class="small-tx-rd">
+              <?php 
+                            echo $sucSave;
+                            echo $saveErorr;
+                            ?>
+            </span>
+          </span>
+        </div>
+      </div>
+    </form>
+    <?php
+        if(isset($_POST['save'])){
+            $checkbox = $_POST['check'];
+            for($i=0;$i<count($checkbox);$i++){
+                $del_id = $checkbox[$i]; 
+                mysqli_query($con,"DELETE FROM products WHERE id='".$del_id."'");
+                $message = "Succses";
+            }
+        }
+        $result = mysqli_query($con,"SELECT * FROM products");
+        if(isset($message)) { 
+        ?>
+
+    <form method="post" action="<?php $_PHP_SELF ?>">
+      <div class="container-menu-cp">
+        <div>
+          <span class="medium-tx">Delete selected:</span><br>
+          <span class="small-tx">select one or more to delete</span>
+        </div>
+        <div>
+          <span class="btn-block">
+            <button type="submit" class="btn-submit" name="save">Delete</button>
+            <span class="small-tx-rd">
+              <?php 
+                                echo $message; 
+                            } 
+                            ?>
+            </span>
+          </span>
+        </div>
+      </div>
+
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Select</th>
+            <th>products Id</th>
+            <th>Date adedd</th>
+            <th>SKU</th>
+            <th>Model</th>
+            <th>Dimension</th>
+            <th>Memory</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <?php
+                $i=0;
+                while($row = mysqli_fetch_array($result)) {
+                    ?>
+        <tr>
+          <td><input type="checkbox" id="checkItem" name="check[]" value="<?php echo $row["id"]; ?>"></td>
+          <td><?php echo $row["id"]; ?></td>
+          <td><?php echo $row["dat"]; ?></td>
+          <td><?php echo $row["sku"]; ?></td>
+          <td><?php echo $row["mox"]; ?></td>
+          <td><?php echo $row["dim"]; ?></td>
+          <td><?php echo $row["mem"]; ?></td>
+          <td><?php echo $row["prc"]; ?></td>
+        </tr>
+        <?php
+                    $i++;
+                }
+                ?>
+      </table>
+    </form>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,165 @@
 <?php
-require_once 'index.view.php';
+session_start([
+    'cookie_lifetime' => 86400,
+    'read_and_close' => true,
+]);
+include_once 'config/app_config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles/app.css">
+  <title>Main : codedbad</title>
+</head>
+
+<body>
+  <header>
+    <a href="https://github.com/codedbad">
+      <div class="codedbad">
+        codedbad
+      </div>
+    </a>
+    <a href="#">
+      <img class="btn-cart" src="images/cart.png" alt="">
+    </a>
+    <a href="index.php">
+      <img class="btn-home" src="images/home.png" alt="">
+    </a>
+  </header>
+  <main>
+    <?php
+        if (isset($_GET['id'])) {
+            $idx = $_GET['id'];
+            $result = mysqli_query($con, "SELECT * FROM products where id=" . $idx);
+            while ($row = mysqli_fetch_array($result))
+            {
+                ?>
+    <div class="container-view">
+      <div class="row-left">
+        <p class="medium-tx">Phone model</p>
+        <p class="big-tx">
+          <?php echo $row['mox']; ?>
+        </p>
+        <p class="small-tx">color version</p><br>
+        <p class="medium-tx">Dimension</p>
+        <p class="big-tx">
+          <?php echo $row['dim']; ?>
+        </p>
+        <p class="small-tx">HxWxD in mm</p><br>
+        <p class="medium-tx">OS</p>
+        <p class="big-tx">
+          <?php echo $row['osx']; ?>
+        </p>
+        <p class="small-tx">operating system</p><br>
+      </div>
+      <div class="row-view">
+        <img src="<?php echo $row['img']; ?>" alt="">
+      </div>
+      <div class="row-right">
+        <p class="medium-tx">Memory</p>
+        <p class="big-tx">
+          <?php echo $row['mem']; ?>
+          <span>Gb</span>
+        </p>
+        <p class="small-tx">built in memory</p><br>
+        <p class="medium-tx">Screen</p>
+        <p class="big-tx">
+          <?php echo $row['scr']; ?>
+        </p>
+        <p class="small-tx">screen resolution</p><br>
+        <p class="medium-tx">Price</p>
+        <p class="price-tag big-tx">
+          <?php echo $row['prc']; ?>
+          <span>Eur</span>
+        </p>
+        <p class="small-tx">including taxes</p>
+        <span class="btn-block">
+          <a class="btn" type="button" href="#">add to Cart</a>
+        </span>
+      </div>
+    </div>
+    <?php
+            }
+        }
+        ?>
+    <div class="container-menu">
+      <div class="row-sort">
+        Sort:
+      </div>
+      <div class="row-list">
+        <select onchange="location = this.value;">
+          <option value="index.php">Newest</option>
+          <option value="index.php">Memory</option>
+          <option value="index.php">Price</option>
+        </select>
+      </div>
+    </div>
+    <div class="container-list">
+      <?php
+            $query = "SELECT * FROM products ORDER by dat DESC";
+            $result = $con->query($query, MYSQLI_STORE_RESULT);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $idx = $row["id"];
+                    $sku = $row["sku"];
+                    $mox = $row["mox"];
+                    $dim = $row["dim"];
+                    $osx = $row["osx"];
+                    $mem = $row["mem"];
+                    $src = $row["scr"];
+                    $prc = $row["prc"];
+                    $img = $row["img"];
+                    ?>
+
+      <div class="row">
+        <ul>
+          <li class="small-tx-cat"><span>Mobile Phones</span></li>
+        </ul>
+        <a class="link" href="index.php?id=<?php echo $idx ?>" rel="">
+          <img src="<?php echo $img; ?>" alt="">
+          <ul>
+            <li class="medium-tx">
+              <span>
+                <?php echo $mox; ?>
+              </span>
+            </li>
+            <li class="small-tx">
+              <span>
+                <?php echo $mem; ?>
+              </span>
+              <span>Gb,</span>
+              <span>
+                <?php echo $src; ?>
+              </span>
+            </li>
+            <li class="price-tag-list">
+              <span>€</span>
+              <span>
+                <?php echo $prc; ?>
+                <span>Eur</span>
+              </span>
+            </li>
+          </ul>
+        </a>
+      </div>
+      <?php
+                }
+            } else {
+                echo "0 results";
+            }
+
+            mysqli_close($con);
+            ?>
+    </div>
+  </main>
+  <footer>
+
+  </footer>
+</body>
+
+</html>
